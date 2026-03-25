121ff17602f3ef9ea3da133fa6d98c70845faaab
Juan Gonzalez <JUAN.GONZALEZMARQUEZ@ucr.ac.cr>
2026-03-24 23:28:23 -0600

    se agrega manejo de error de entrada

--- Proyecto/Etapa 1/cliente/ClienteMain.cc
+++ Proyecto/Etapa 1/cliente/ClienteMain.cc
@@ -1,15 +1,23 @@
 #include "Cliente.h"
 #include <iostream>
+#include <string>
 
 int main(int argc, char * argv[]) {
-    char continuar;
+    bool useSSL = (argc > 1);
+    std::string input;
+
     do {
-        Cliente cliente;
+        Cliente cliente(useSSL);
         cliente.ejecutar();
-        
+
         std::cout << "¿Desea solicitar otra figura? (s/n): ";
-        std::cin >> continuar;
-    } while (continuar == 's' || continuar == 'S');
-    
+        std::getline(std::cin, input);
+
+        if (input.empty()) {
+            input = "n";
+        }
+
+    } while (input == "s" || input == "S");
+
     return 0;
 }
